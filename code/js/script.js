1032d64f86b7cdf4d30dffe559ec912bd7706102
Shishir <119591117+Shishir3k@users.noreply.github.com>
2024-01-10 16:18:50 +0530

    resources uploaded

---
+++ code/js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".auto-input", {
+  strings: [
+    "Programmer",
+    "Web Developer",
+    "Python Developer",
+    "Java Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 30,
+  loop: true,
+});
